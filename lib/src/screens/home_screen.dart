24ae2a9b7584a46324bbe4207261d578f0ecd372
rishavk1102 <rishavk1102@gmail.com>
2020-04-30 14:23:38 +0530

    Added functionality to BottomNavigation

--- lib/src/screens/home_screen.dart
+++ lib/src/screens/home_screen.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-import '../custom_icons/home_filled_icons.dart';
+import '../custom_icons/custom_icons.dart';
 import '../utils/ui_image.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -11,6 +10,8 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  int currentIndex = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,9 +24,9 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget buildAppBar(BuildContext context) {
     return AppBar(
       leading: IconButton(
-        padding: EdgeInsets.all(12.0),
-        icon: SvgPicture.asset(
-          UiImage.camera,
+        icon: Icon(
+          CustomIcons.photo_camera,
+          size: 32.0,
           color: Colors.black,
         ),
         onPressed: () {},
@@ -43,9 +44,8 @@ class _HomeScreenState extends State<HomeScreen> {
       ),
       actions: <Widget>[
         IconButton(
-          padding: EdgeInsets.all(12.0),
-          icon: SvgPicture.asset(
-            UiImage.igtv,
+          icon: Icon(
+            CustomIcons.igtv,
             color: Colors.black,
           ),
           onPressed: () {},
@@ -53,15 +53,14 @@ class _HomeScreenState extends State<HomeScreen> {
         Transform.rotate(
           angle: 0.4,
           child: IconButton(
-            padding: EdgeInsets.all(12.0),
-            icon: SvgPicture.asset(
-              UiImage.send,
+            icon: Icon(
+              CustomIcons.paper_plane,
               color: Colors.black,
             ),
             onPressed: () {},
           ),
         ),
-        SizedBox(width: 8.0),
+        SizedBox(width: 12.0),
       ],
     );
   }
@@ -73,33 +72,50 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget buildBottomNavigationBar() {
     final List<BottomNavigationBarItem> items = [
       BottomNavigationBarItem(
-        icon: Icon(HomeFilled.home_filled),
+        icon: Icon(currentIndex == 0
+            ? CustomIcons.home_filled
+            : CustomIcons.home_lineal),
         title: Text(''),
       ),
       BottomNavigationBarItem(
-        icon: Icon(Icons.search),
+        icon: Icon(currentIndex == 1
+            ? CustomIcons.search_fill
+            : CustomIcons.search_lineal),
         title: Text(''),
       ),
       BottomNavigationBarItem(
-        icon: Icon(Icons.add_box),
+        icon: Icon(CustomIcons.add),
         title: Text(''),
       ),
       BottomNavigationBarItem(
-        icon: Icon(FontAwesomeIcons.heart),
+        icon: Icon(currentIndex == 3
+            ? CustomIcons.like_fill
+            : CustomIcons.like_lineal),
         title: Text(''),
       ),
       BottomNavigationBarItem(
-        icon: Icon(FontAwesomeIcons.peopleCarry),
+        icon: Icon(currentIndex == 4
+            ? CustomIcons.people_fill
+            : CustomIcons.profile_lineal),
         title: Text(''),
       ),
     ];
 
     return BottomNavigationBar(
       items: items,
-      backgroundColor: Colors.blue,
       elevation: 20.0,
-      selectedIconTheme: IconThemeData(color: Colors.black),
-      unselectedIconTheme: IconThemeData(color: Colors.black54),
+      onTap: (index) {
+        setState(() {
+          currentIndex = index;
+        });
+      },
+      currentIndex: currentIndex,
+      iconSize: 24.0,
+      showSelectedLabels: false,
+      selectedItemColor: Colors.black,
+      unselectedItemColor: Colors.black,
+      selectedFontSize: 0.0,
+      type: BottomNavigationBarType.fixed,
     );
   }
 }
