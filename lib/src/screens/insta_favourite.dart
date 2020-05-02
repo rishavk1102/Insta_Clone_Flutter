da629f94d1010e995daf45aa227e10e5cfa1e454
rishavk1102 <rishavk1102@gmail.com>
2020-05-03 00:41:10 +0530

    FavouritesFragment UI

--- lib/src/screens/insta_favourite.dart
+++ lib/src/screens/insta_favourite.dart
@@ -1,12 +1,90 @@
 import 'package:flutter/material.dart';
 
-class InstaFavourite extends StatelessWidget {
+class InstaFavourite extends StatefulWidget {
+  @override
+  _InstaFavouriteState createState() => _InstaFavouriteState();
+}
+
+class _InstaFavouriteState extends State<InstaFavourite>
+    with SingleTickerProviderStateMixin {
+  TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(vsync: this, length: 2, initialIndex: 0);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Text('Favourites'),
+    return SafeArea(
+      child: Scaffold(
+        body: NestedScrollView(
+          headerSliverBuilder: (BuildContext context, bool isBoxScrolled) =>
+              <Widget>[
+            SliverPersistentHeader(
+              delegate: CustomSliverDelegate(_tabController),
+              pinned: true,
+            ),
+          ],
+          body: TabBarView(
+            controller: _tabController,
+            children: <Widget>[
+              Center(
+                child: Text('Tab1'),
+              ),
+              Center(
+                child: Text('Tab2'),
+              ),
+            ],
+          ),
+        ),
       ),
     );
+  }
+}
+
+class CustomSliverDelegate extends SliverPersistentHeaderDelegate {
+  TabController _tabController;
+
+  CustomSliverDelegate(this._tabController);
+
+  @override
+  Widget build(
+      BuildContext context, double shrinkOffset, bool overlapsContent) {
+    return Container(
+      height: 50.0,
+      decoration: BoxDecoration(
+        color: Colors.grey[50],
+      ),
+      child: TabBar(
+        controller: _tabController,
+        indicatorColor: Colors.black,
+        unselectedLabelColor: Colors.black38,
+        tabs: <Widget>[
+          Tab(
+            child: Text(
+              'Following'.toUpperCase(),
+            ),
+          ),
+          Tab(
+            child: Text(
+              'You'.toUpperCase(),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  double get maxExtent => 50.0;
+
+  @override
+  double get minExtent => 50.0;
+
+  @override
+  bool shouldRebuild(SliverPersistentHeaderDelegate oldDelegate) {
+    return false;
   }
 }
